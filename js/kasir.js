2dae7cfc606f7b29f0d4c6125bd72ec36b5180df
desibebek99-hash <desibebek99@gmail.com>
2026-06-26 15:02:34 +0700

    Update kasir.js

--- js/kasir.js
+++ js/kasir.js
@@ -328,10 +328,24 @@ function detail(index){
     .style.display="flex";
 
 }
+
 function closeModal(){
 
     document
     .getElementById("modal")
     .style.display="none";
+
+}
+
+window.onclick=function(event){
+
+    let modal =
+    document.getElementById("modal");
+
+    if(event.target===modal){
+
+        closeModal();
+
+    }
 
 }
