81ddd9a39a80fd5467532a6ee9a2a53011129827
desibebek99-hash <desibebek99@gmail.com>
2026-06-26 14:01:37 +0700

    Update kasir.js

--- js/kasir.js
+++ js/kasir.js
@@ -1,3 +1,118 @@
-let orders = getOrders();
+const dashboard =
+document.getElementById("dashboard");
 
-console.log(orders);
+renderDashboard();
+
+function renderDashboard(){
+
+    let orders = getOrders();
+
+    let totalOrder = orders.length;
+
+    let omzet = 0;
+
+    orders.forEach(order=>{
+
+        omzet += order.total;
+
+    });
+
+    let html = `
+
+<div class="summary-grid">
+
+<div class="summary-card">
+
+<div class="summary-title">
+
+Order Hari Ini
+
+</div>
+
+<div class="summary-value">
+
+${totalOrder}
+
+</div>
+
+</div>
+
+<div class="summary-card">
+
+<div class="summary-title">
+
+Omzet
+
+</div>
+
+<div class="summary-value">
+
+Rp${omzet.toLocaleString("id-ID")}
+
+</div>
+
+</div>
+
+</div>
+
+`;
+
+orders.reverse().forEach(order=>{
+
+html += `
+
+<div class="order-card">
+
+<div class="order-title">
+
+${order.customer}
+
+</div>
+
+<br>
+
+Meja ${order.table}
+
+<br><br>
+
+Status
+
+<b>${order.status}</b>
+
+<div class="order-total">
+
+Rp${order.total.toLocaleString("id-ID")}
+
+</div>
+
+<div class="order-buttons">
+
+<button class="btn">
+
+✔ Bayar
+
+</button>
+
+<button class="btn">
+
+🧾 Nota
+
+</button>
+
+<button class="btn">
+
+👁 Detail
+
+</button>
+
+</div>
+
+</div>
+
+`;
+
+});
+
+dashboard.innerHTML = html;
+
+}
