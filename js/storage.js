5727d562c7744853eaaa53f23146de8e4cb836fc
desibebek99-hash <desibebek99@gmail.com>
2026-06-26 15:23:20 +0700

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -3,145 +3,72 @@
 // =========================
 
 // ORDER
-
-function getOrders(){
-
-    return JSON.parse(
-
-        localStorage.getItem("orders")
-
-    ) || [];
-
+function getOrders() {
+    return JSON.parse(localStorage.getItem("orders")) || [];
 }
 
-function saveOrders(data){
-
-    localStorage.setItem(
-
-        "orders",
-
-        JSON.stringify(data)
-
-    );
-
+function saveOrders(data) {
+    localStorage.setItem("orders", JSON.stringify(data));
 }
 
 // MENU
-
-function getMenus(){
-
-    return JSON.parse(
-
-        localStorage.getItem("menus")
-
-    ) || [];
-
+function getMenus() {
+    return JSON.parse(localStorage.getItem("menus")) || [];
 }
 
-function saveMenus(data){
-
-    localStorage.setItem(
-
-        "menus",
-
-        JSON.stringify(data)
-
-    );
-
+function saveMenus(data) {
+    localStorage.setItem("menus", JSON.stringify(data));
 }
 
 // SETTING
-
-function getSetting(){
-
-    return JSON.parse(
-
-        localStorage.getItem("setting")
-
-    ) || {};
-
+function getSetting() {
+    return JSON.parse(localStorage.getItem("setting")) || {};
 }
 
-function saveSetting(data){
-
-    localStorage.setItem(
-
-        "setting",
-
-        JSON.stringify(data)
-
-    );
-
+function saveSetting(data) {
+    localStorage.setItem("setting", JSON.stringify(data));
 }
 
-if(getOrders().length===0){
+// DATA CONTOH
+if (getOrders().length === 0) {
 
-saveOrders([
+    saveOrders([
 
-{
+        {
+            id: 1,
+            customer: "Andi",
+            table: 5,
+            status: "Menunggu Bayar",
+            total: 75000,
+            items: [
+                {
+                    name: "Nasi Goreng",
+                    qty: 2,
+                    price: 25000
+                },
+                {
+                    name: "Es Teh",
+                    qty: 1,
+                    price: 25000
+                }
+            ]
+        },
 
-id:1,
+        {
+            id: 2,
+            customer: "Budi",
+            table: 2,
+            status: "Diproses",
+            total: 56000,
+            items: [
+                {
+                    name: "Mie Ayam",
+                    qty: 2,
+                    price: 28000
+                }
+            ]
+        }
 
-customer:"Andi",
-
-table:5,
-
-status:"Menunggu Bayar",
-
-total:75000,
-
-items:[
-
-{
-
-name:"Nasi Goreng",
-
-qty:2,
-
-price:25000
-
-},
-
-{
-
-name:"Es Teh",
-
-qty:1,
-
-price:25000
-
-}
-
-]
-
-},
-
-{
-
-id:2,
-
-customer:"Budi",
-
-table:2,
-
-status:"Diproses",
-
-total:56000,
-
-items:[
-
-{
-
-name:"Mie Ayam",
-
-qty:2,
-
-price:28000
+    ]);
 
 }
-
-]
-
-}
-
-]);
